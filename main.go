af86638580e1d986e42a4a48cd1941c3cf66c1c8
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-09-10 21:08:56 +0700

    feat: Return hello world in the root url

---
+++ main.go
@@ -0,0 +1,18 @@
+// main.go
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
